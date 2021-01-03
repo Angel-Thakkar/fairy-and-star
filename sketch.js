e1b1d52b5c9372d766cde147a591a5c867fd9902
Angel-Thakkar <75518665+Angel-Thakkar@users.noreply.github.com>
2021-01-03 17:29:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  if(keyDown("left")){
+	  fairy.x -=3
+  }
+  if(keyDown("right")){
+	fairy.x +=3
+}
+if(keyDown("DOWN_ARROW")){
+	star.velocityY +=3
+}
+  drawSprites();
+
+}
+
+function keyPressed() {
+	//write code here
+}
